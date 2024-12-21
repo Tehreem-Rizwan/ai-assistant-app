fec3653c6d25dd84f3ebaa631936c533e0a409d6
Tehreem Rizwan <its.tehreem2830@gmail.com>
2024-12-21 22:10:15 +0500

    update

--- lib/controllers/image_controller.dart
+++ lib/controllers/image_controller.dart
@@ -1,6 +1,14 @@
+import 'package:ai_assistant/screens/helper/global.dart';
+import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ImageController extends GetxController {
   final textC = TextEditingController();
+  Future<void> createAIImage() async {
+    OpenAI.apiKey = apiKey;
+    if (textC.text.trim().isNotEmpty) {
+      textC.text;
+    }
+  }
 }
