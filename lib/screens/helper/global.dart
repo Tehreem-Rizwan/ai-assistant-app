48d53baf9e64f45be5dbbbee83ec74a028ee43f6
Tehreem Rizwan <its.tehreem2830@gmail.com>
2024-12-21 22:11:40 +0500

    Update global.dart

--- lib/screens/helper/global.dart
+++ lib/screens/helper/global.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 const appName = "AI Assistant";
 late Size mq;
-const apiKey = "AIzaSyAkr6MpN1El7InLaOzy7t94xbUoFxlFwyQ";
+const apiKey = "";
 
-   // "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";
+   
